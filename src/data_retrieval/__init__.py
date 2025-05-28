4e56b0ef51509ec833e56363d5ecf239d925109d
Yasinus <yasinkirschstein@gmail.com>
2025-05-28 21:56:40 +0200

    simple structure testing

---
+++ src/data_retrieval/__init__.py
@@ -0,0 +1,3 @@
+from . import detection_mechanism
+from . import text_to_speech
+from . import warning_generator
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+from typing import Dict, Any
+
+def test_function():
+    return "Hello world"
